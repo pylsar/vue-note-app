ed833b30c5d5732e54f428b7d361a0440a151b1e
Alex Kusov <kysov2006@bk.ru>
2024-09-13 06:02:35 +0300

    addNote start

---
+++ src/components/common/AddNote.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {ref} from 'vue';
+import contenteditable from 'vue-contenteditable';
+
+const title = ref('');
+const content = ref('');
+
+const handleForm = (e) => {
+    console.log(e)
+}
+</script>
+
+<template>
+    <div class="add-note">
+        <form @submit.prevent="handleForm">
+            <input type="text" placeholder="what is it about" v-model="title">
+
+            <contenteditable
+                tag="div"
+                :contenteditable="true"
+                :no-html="true"
+                :no-nl="false"
+                v-model="content"
+            />
+
+            <button><span>Сохранить</span></button>
+        </form>
+    </div>    
+</template>
--- src/components/list/ListView.vue
+++ src/components/list/ListView.vue
@@ -1,7 +1,9 @@
 <script setup>
-import Sidebar from './Sidebar.vue'
+    import Sidebar from './Sidebar.vue';
+    import AddNote from '../common/AddNote.vue';
 </script>
 
 <template>
     <Sidebar/>
+    <AddNote />
 </template>
